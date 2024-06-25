ca9e662049b992a05249ec214bcfab0c9f17d93f
fuokuyama16 <fuokuyama16@gmail.com>
2024-06-25 20:07:12 +0900

    プルダウンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root to: "messages#index"
 resources :users, only: [:edit, :update]
+resources :rooms, only: [:new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
