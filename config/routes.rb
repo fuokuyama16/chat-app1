b032769bab9c16b2f01145ddfd07d1e85726148b
fuokuyama16 <fuokuyama16@gmail.com>
2024-06-26 20:05:52 +0900

    チャットルームの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  root to: "rooms#index"
 resources :users, only: [:edit, :update]
-resources :rooms, only: [:new, :create]
-  # Defines the root path route ("/")
-  # root "articles#index"
+resources :rooms, only: [:new, :create] do
+  resources :messages, only: [:index, :create]
+end
 end
